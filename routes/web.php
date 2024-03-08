56507173ae5fb34200d8c36d018444e0fa98ad8a
Jeffrey Way <jeffrey@jeffrey-way.com>
2024-03-08 13:45:07 -0500

    Day 2 Complete

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
