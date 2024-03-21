2bc43246090afb7dfaeaea80bdd905bfccf6ba3e
Jeffrey Way <jeffrey@jeffrey-way.com>
2024-03-21 15:06:14 -0400

    Day 8 Complete

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
