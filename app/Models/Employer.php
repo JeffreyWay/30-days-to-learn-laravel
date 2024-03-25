c29d583f5155c36de48abbcc424b743f89ad6544
Jeffrey Way <jeffrey@jeffrey-way.com>
2024-03-25 15:26:27 -0400

    Day 11 Complete

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Employer extends Model
 {
     use HasFactory;
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
+    use HasFactory;
+
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
