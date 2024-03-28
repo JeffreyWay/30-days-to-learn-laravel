15ff8063f8d1fab674eee138ad30bce2376e2311
Jeffrey Way <jeffrey@jeffrey-way.com>
2024-03-28 12:03:27 -0400

    Day 13 Complete

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,15 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+
+                <div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->get();
+
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs
     ]);
 });
 
